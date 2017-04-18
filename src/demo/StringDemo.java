c395e07b6cde5d819397b689a800f0187e5650cf
546361420@qq.com <546361420@qq.com>
2017-04-18 11:49:41 +0800

    asd asasd

---
+++ src/demo/StringDemo.java
@@ -0,0 +1,96 @@
+package demo;
+
+public class StringDemo {
+	public StringDemo() {
+	}
+	public String smallString(String str)
+	{
+		String temp = "error";
+		if (str.equals("") || str == null) {
+			return temp;
+		}
+		String s = str.toLowerCase();
+		return s;
+	}
+	public String smallString1(String str)
+	{
+		String temp = "error";
+		if (str.equals("") || str == null) {
+			return temp;
+		}
+		String s = str.toLowerCase();
+		return s;
+	}
+	public String smallString2(String str)
+	{
+		String temp = "error";
+		if (str.equals("") || str == null) {
+			return temp;
+		}
+		String s = str.toLowerCase();
+		return s;
+	}
+	public String smallString3(String str)
+	{
+		String temp = "error";
+		if (str.equals("") || str == null) {
+			return temp;
+		}
+		String s = str.toLowerCase();
+		return s;
+	}
+	public String smallString4(String str)
+	{
+		String temp = "error";
+		if (str.equals("") || str == null) {
+			return temp;
+		}
+		String s = str.toLowerCase();
+		return s;
+	}
+	public String smallString5(String str)
+	{
+		String temp = "error";
+		if (str.equals("") || str == null) {
+			return temp;
+		}
+		String s = str.toLowerCase();
+		return s;
+	}
+	public String smallString6(String str)
+	{
+		String temp = "error";
+		if (str.equals("") || str == null) {
+			return temp;
+		}
+		String s = str.toLowerCase();
+		return s;
+	}
+	public String smallString7(String str)
+	{
+		String temp = "error";
+		if (str.equals("") || str == null) {
+			return temp;
+		}
+		String s = str.toLowerCase();
+		return s;
+	}
+	public String smallString8(String str)
+	{
+		String temp = "error";
+		if (str.equals("") || str == null) {
+			return temp;
+		}
+		String s = str.toLowerCase();
+		return s;
+	}
+	public String smallString9(String str)
+	{
+		String temp = "error";
+		if (str.equals("") || str == null) {
+			return temp;
+		}
+		String s = str.toLowerCase();
+		return s;
+	}
+}
---
+++ src/demo/testStringDemo1.java
@@ -0,0 +1,51 @@
+package demo;
+import demo.StringDemo;
+import junit.framework.TestCase;
+
+public class testStringDemo1 extends TestCase {
+
+	private StringDemo str;
+	protected void setUp(){
+		str = new StringDemo();
+	}
+		public void testSmallString() {
+		assertEquals("1",str.smallString("A"),"a");
+		
+	}
+		public void testSmallString1() {
+			assertEquals("1",str.smallString("A"),"a");
+			
+		}
+		public void testSmallString2() {
+			assertEquals("1",str.smallString("A"),"a");
+			
+		}
+		public void testSmallString3() {
+			assertEquals("1",str.smallString("A"),"a");
+			
+		}
+		public void testSmallString4() {
+			assertEquals("1",str.smallString("A"),"a");
+			
+		}
+		public void testSmallString5() {
+			assertEquals("1",str.smallString("A"),"a");
+			
+		}
+		public void testSmallString6() {
+			assertEquals("1",str.smallString("A"),"a");
+			
+		}
+		public void testSmallString7() {
+			assertEquals("1",str.smallString("A"),"a");
+			
+		}
+		public void testSmallString8() {
+			assertEquals("1",str.smallString("A"),"a");
+			
+		}
+		public void testSmallString9() {
+			assertEquals("1",str.smallString("A"),"a");
+			
+		}
+}
